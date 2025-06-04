73d05e7e920b3e3f1123e2b2a2059ee5825a3590
TheCopyNinjaDev <thecopyninjadev@gmail.com>
2025-06-04 14:27:52 +0300

    Fix webhook server return types

--- backend/services/webhook_server.py
+++ backend/services/webhook_server.py
@@ -1,4 +1,5 @@
 import httpx
+from typing import Any, Dict
 
 async def get_bot_id(token: str) -> int:
     async with httpx.AsyncClient() as client:
@@ -7,7 +8,7 @@ async def get_bot_id(token: str) -> int:
         return data["result"]["id"]
 
 
-async def set_webhook(TOKEN: str, WEBHOOK_URL: str) -> int:
+async def set_webhook(TOKEN: str, WEBHOOK_URL: str) -> Dict[str, Any]:
     async with httpx.AsyncClient() as client:
         response = await client.post(
             f'https://api.telegram.org/bot{TOKEN}/setWebhook',
@@ -20,7 +21,7 @@ async def set_webhook(TOKEN: str, WEBHOOK_URL: str) -> int:
         }
     
 
-async def delete_webhook(token: str):
+async def delete_webhook(token: str) -> Dict[str, Any]:
     url = f"https://api.telegram.org/bot{token}/deleteWebhook"
     async with httpx.AsyncClient() as client:
         response = await client.post(url)
