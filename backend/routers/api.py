c1cd2bfadda4e032b4ae01e696f369dcae2d032d
TheCopyNinjaDev <thecopyninjadev@gmail.com>
2025-06-17 18:54:53 +0300

    feat(api): send integration request only when bot is new

--- backend/routers/api.py
+++ backend/routers/api.py
@@ -8,13 +8,19 @@ from constants.response_models import (
     IntegrationResponse,
     UsersPageResponse,
 )
-from config.settings import INTEGRATION_URL, INTEGRATION_CODE, INTEGRATION_TOKEN, WEBHOOK_URL
+from config.settings import (
+    INTEGRATION_URL,
+    INTEGRATION_CODE,
+    INTEGRATION_TOKEN,
+    WEBHOOK_URL,
+)
 import httpx
 import services.helper_functions as hf
 import services.db as db
 from services.webhook_server import get_bot_name, get_bot_id, set_webhook
 
 router = APIRouter(prefix="/api", tags=["API"])
+
 
 @router.post("/bot", response_model=IntegrationResponse)
 async def integrate_new_user(request: IntegrateRequest):
@@ -26,55 +32,75 @@ async def integrate_new_user(request: IntegrateRequest):
 
     if not bot_id or not bot_name:
         raise HTTPException(status_code=401, detail="Invalid bot token")
-    
-    if db.bot_exists(bot_id):
+
+    bot_exists = db.bot_exists(bot_id)
+
+    if not bot_exists:
+        pass_uuid = hf.generate_uuid()
+
+        set_webhook_response = await set_webhook(
+            token, f"{WEBHOOK_URL}/webhook/{bot_id}"
+        )
+
+        if set_webhook_response["status_code"] != 200:
+            raise HTTPException(status_code=500, detail="Failed to set webhook")
+    else:
         stored_name, pass_uuid, web_url = db.get_bot_auth(bot_id)
 
-        return IntegrationResponse(botName=stored_name, passUuid=pass_uuid, webUrl=web_url, botId=bot_id)
-    
-    pass_uuid = hf.generate_uuid()
-
-    set_webhook_response = await set_webhook(token, f"{WEBHOOK_URL}/webhook/{bot_id}")
-
-    if set_webhook_response["status_code"] != 200:
-        raise HTTPException(status_code=500, detail="Failed to set webhook")
-
     integration_request = {
-        "externalId": "12", # TODO replace with bot_id
+        "externalId": "12",  # TODO replace with bot_id
         "name": str(bot_name),
         "locale": "ru",
         "paymentType": "external",
         "status": {
             "status": "active",
-        }
+        },
     }
 
     headers = {
         "Authorization": f"Bearer {INTEGRATION_TOKEN}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     try:
-        async with httpx.AsyncClient() as client:
-            integration_response = await client.post(f"{INTEGRATION_URL}/{INTEGRATION_CODE}",
-            json=integration_request,
-            headers=headers
+        if not bot_exists:
+            async with httpx.AsyncClient() as client:
+                integration_response = await client.post(
+                    f"{INTEGRATION_URL}/{INTEGRATION_CODE}",
+                    json=integration_request,
+                    headers=headers,
+                )
+
+            data = integration_response.json()
+
+            if "webUrl" in data:
+                web_url = data["webUrl"]
+            else:
+                try:
+                    error_detail = integration_response.json()
+                except Exception:
+                    error_detail = integration_response.text
+                raise HTTPException(
+                    status_code=integration_response.status_code,
+                    detail=error_detail,
+                )
+
+            db.create_new_bot(
+                bot_id,
+                token,
+                bot_name,
+                owner_uuid,
+                pass_uuid,
+                web_url,
             )
 
-        data = integration_response.json()
-
-        if "webUrl" in data:
-            web_url = data["webUrl"]
+            return IntegrationResponse(
+                botName=bot_name, passUuid=pass_uuid, webUrl=web_url, botId=bot_id
+            )
         else:
-            try:
-                error_detail = integration_response.json()
-            except Exception:
-                error_detail = integration_response.text
-            raise HTTPException(status_code=integration_response.status_code, detail=error_detail)
-        
-        db.create_new_bot(bot_id, token, bot_name, owner_uuid, pass_uuid, web_url)
-
-        return IntegrationResponse(botName=bot_name, passUuid=pass_uuid, webUrl=web_url, botId=bot_id)
+            return IntegrationResponse(
+                botName=stored_name, passUuid=pass_uuid, webUrl=web_url, botId=bot_id
+            )
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Request failed: {str(e)}")
@@ -96,7 +122,8 @@ async def bots_by_owner(owner_uuid: str):
         ]
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-        
+
+
 @router.get("/{bot_id}/isVerified")
 async def is_bot_verified(bot_id: int):
     return db.is_bot_verified(bot_id)
@@ -123,6 +150,7 @@ async def list_bot_users(
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @router.get("/{bot_id}/owner")
 async def owner_name(bot_id: int) -> str:
     try:
@@ -131,6 +159,7 @@ async def owner_name(bot_id: int) -> str:
         return owner_name
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.get("/{bot_id}/authInfo")
 async def auth_info(bot_id: int):
@@ -151,6 +180,7 @@ async def logout_owner(bot_id: int):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @router.patch("/bot/{bot_id}/user/{user_id}")
 async def switch_activness(bot_id: int, user_id: int, new_status: bool):
     try:
@@ -158,6 +188,7 @@ async def switch_activness(bot_id: int, user_id: int, new_status: bool):
         return {"user_id": user_id, "is_active": new_status}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.delete("/bot/{bot_id}/user/{user_id}")
 async def delete_user(bot_id: int, user_id: int):
